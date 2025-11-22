3a84ecc9ca8cb5f7f0a2c529dbbb1d613191a2ea
Ywoswho <fckdyth@gmail.com>
2025-11-22 19:14:18 +0200

    Initial commit

---
+++ src/main/java/net/ywoswho/magiac/Magiac.java
@@ -0,0 +1,17 @@
+package net.ywoswho.magiac;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Magiac implements ModInitializer {
+	public static final String MOD_ID = "magiac";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/ywoswho/magiac/MagiacClient.java
@@ -0,0 +1,10 @@
+package net.ywoswho.magiac;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagiacClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
